e7315eb1d60d28fc7f9c98aafdf68f0406c19182
Marvin <callof61@gmail.com>
2015-11-13 11:58:58 +0100

    ajout des deux page de traitement de l'inscription

--- contents/register.php
+++ contents/register.php
@@ -1,21 +1,25 @@
 <section class="login left">
 		<div class="border-top red"></div>
-		<h1>Login </h1>
+		<h1 class="fred">Login </h1>
 		<form method="post" action="connexion.php">
 		<p> 
 			Pseudo : <input type="text"/></br>
-			Mot de passe : <input type="password"/>
+			Mot de passe : <input type="password"/></br>
+			<input type="submit" value="Valider" />
+
 		 </p>
 		
 </section>
 <section class="register right">
 		<div class="border-top green"></div>
-		<h1> S'inscrire </h1>
+		<h1 class="fgreen"> S'inscrire </h1>
 		<form method="post" action="inscription.php">
 		<p> 
 			Pseudo : <input type="text"/></br>
 			Adresse mail : <input type="text"/></br>
 			Mot de passe <input type="password"/></br>
+			<input type="submit" value="Valider" />
+
 			
 		</p>
 </section>
