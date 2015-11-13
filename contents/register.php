6c5add6172082feb3c6423787a3e89645b7badc0
Marvin <callof61@gmail.com>
2015-11-13 11:44:01 +0100

    Creation de la page d'inscription + creation de classe pour les bloc login et register

---
+++ contents/register.php
@@ -0,0 +1,22 @@
+<section class="login left">
+		<div class="border-top red"></div>
+		<h1>Login </h1>
+		<form method="post" action="connexion.php">
+		<p> 
+			Pseudo : <input type="text"/></br>
+			Mot de passe : <input type="password"/>
+		 </p>
+		
+</section>
+<section class="register right">
+		<div class="border-top green"></div>
+		<h1> S'inscrire </h1>
+		<form method="post" action="inscription.php">
+		<p> 
+			Pseudo : <input type="text"/></br>
+			Adresse mail : <input type="text"/></br>
+			Mot de passe <input type="password"/></br>
+			
+		</p>
+</section>
+		
