2e00a186143d8db689777120b4e1b09308a5fc6a
Brandon/DanAurea <laochi.bc@gmail.com>
2015-11-16 02:22:36 +0100

    Modification responsive design

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 	 		?>
 		</title>
 		<link rel="stylesheet" type="text/css" href='<?php echo BASE_URL.'/css/style.css'?>' />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.5">
 	</head>
 	 
 	<body>	
